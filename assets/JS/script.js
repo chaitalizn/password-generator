ccca165b7aa33c7ae8558c149b959519856c8266
chaitalizn <phchaitali3@gmail.com>
2021-03-13 15:11:13 -0500

    create prompts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,35 @@
 // Assignment code here
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNIPQRATUVWXYZ";
+var number = "0123456789";
+var symbol = " !@#$%^&*(){}[]=<>/,.";
+
+
+function generatePassword(passwordLength, characterTypes){
+  var pwd = "";
+  for(var i = 0; i < passwordLength; i++){
+    pwd += characterTypes.charAt(Math.floor(Math.random()* characterTypes.length));
+  }
+};
+
+
+var passwordLength = Number(prompt("How many character would you like your password to be?"));
+
+if (passwordLength < 8 || passwordLength > 128) {
+  alert ("Please enter password length between 8 and 128 characters.")
+}
+else {
+var uppercase = confirm("Would you like to include uppercase?");
+var lowercase = confirm("would you like to include lowercase?");
+var number = confirm("Would you like to include numbers?");
+var specialCharacters = confirm("Would you like to include special characters?");
+
+if (!uppercase && !lowercase && !number && !specialCharacters) {
+  alert ("Please at least choose 1 character type.");
+};
+};
+
+
 
 
 // Get references to the #generate element
@@ -15,3 +46,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
