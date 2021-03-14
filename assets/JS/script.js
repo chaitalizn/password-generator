9a2348ac9d9ff764d33eb1a93f7cb8038899811a
chaitalizn <phchaitali3@gmail.com>
2021-03-13 23:35:41 -0500

    generatePassword function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,36 +1,54 @@
 // Assignment code here
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNIPQRATUVWXYZ";
-var number = "0123456789";
-var symbol = " !@#$%^&*(){}[]=<>/,.";
+//all possible password values variable
+var lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseSet = "ABCDEFGHIJKLMNIPQRATUVWXYZ";
+var numberSet = "0123456789";
+var symbolSet = "!@#$%^&*(){}[]=<>/,.";
 
+function generatePassword() {
 
-function generatePassword(passwordLength, characterTypes){
-  var pwd = "";
-  for(var i = 0; i < passwordLength; i++){
-    pwd += characterTypes.charAt(Math.floor(Math.random()* characterTypes.length));
-  }
-};
+//prompt for password character length
+var passwordLength = Number(prompt("How many character would you like your password to be? Choose between 8 to 128"));
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = Number(prompt("How many character would you like your password to be? Choose between 8 to 128"));
+  }  
 
-
-var passwordLength = Number(prompt("How many character would you like your password to be?"));
-
-if (passwordLength < 8 || passwordLength > 128) {
-  alert ("Please enter password length between 8 and 128 characters.")
-}
-else {
+//confirm character type password to generate     
 var uppercase = confirm("Would you like to include uppercase?");
-var lowercase = confirm("would you like to include lowercase?");
+var lowercase = confirm("Would you like to include lowercase?");
 var number = confirm("Would you like to include numbers?");
 var specialCharacters = confirm("Would you like to include special characters?");
+  while (!uppercase && !lowercase && !number && !specialCharacters) {
+    alert ("Please select one character type.");
+    var uppercase = confirm("Would you like to include uppercase?");
+    var lowercase = confirm("Would you like to include lowercase?");
+    var number = confirm("Would you like to include numbers?");
+    var specialCharacters = confirm("Would you like to include special characters?");
+  };
 
-if (!uppercase && !lowercase && !number && !specialCharacters) {
-  alert ("Please at least choose 1 character type.");
+// concat all character type user confirm and store in passwordString
+var passwordString = "";
+
+if (uppercase) {
+  passwordString = passwordString.concat(uppercaseSet);
+}
+if (lowercase) {
+  passwordString = passwordString.concat(lowercaseSet);
+}
+if (number) {
+  passwordString = passwordString.concat(numberSet);
+}
+if (specialCharacters) {
+  passwordString = passwordString.concat(symbolSet);
+}
+
+//genereate random password from the passwordString
+var passwordRandom = "";
+for (var i = 0; i < passwordLength; i++) {
+      passwordRandom += passwordString.charAt(Math.floor(Math.random()* passwordString.length));
+  }
+  return passwordRandom;
 };
-};
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -46,5 +64,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
